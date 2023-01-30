43a1a12c32ea1372a99779fb5472e59d76406a2b
wahidrana2108 <wahidzamanrana@gmail.com>
2023-01-30 19:31:30 +0600

    .

--- text.php
+++ text.php
@@ -5,7 +5,7 @@
 <form action="#" method="post">
     <input type="email" name="user" placeholder="Enter username" required><br><br>
     <input type="text" name="pass" placeholder="Enter Password" required><br><br>
-    <input type="submit" value="Insert" name="login">
+    <input type="submit" value="Insert" name="Login">
 </form>
 
 
